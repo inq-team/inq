a6d612ca4ba40300bff72150c35e88584aadebae
Mikhail Yakshin <greycat@altlinux.org>
2007-10-02 14:20:24 +0000

    Added Order model
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@37 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/models/order.rb
+++ server/web/app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ActiveRecord::Base
+	has_many :order_lines
+	belongs_to :manager, :class_name => 'Person', :foreign_key => 'manager_id'
+
+	def update_order(attr)
+		order_lines = attr[:order_lines]
+		attr.delete(:order_lines)
+		p order_lines
+		update_attributes(attr)
+	end
 end
