01d7816b2873d5ddd35181ce2cc9a4bf9daccc4f
Mikhail Yakshin <greycat@altlinux.org>
2007-10-02 14:21:09 +0000

    Some Computers REST implementation
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@39 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/controllers/computers_controller.rb
+++ server/web/app/controllers/computers_controller.rb
@@ -4,8 +4,8 @@ class ComputersController < ApplicationController
 	end
 
 	# GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-	verify :method => :post, :only => [ :destroy, :create, :update ],
-	       :redirect_to => { :action => :archive }
+#	verify :method => :post, :only => [ :destroy, :create, :update ],
+#	       :redirect_to => { :action => :archive }
 
 	def archive
 		@computer_pages, @computers = paginate :computers, :per_page => 20
@@ -18,7 +18,7 @@ class ComputersController < ApplicationController
 		@components = @testing.components
 		respond_to { |format|
 			format.html { render :action => 'testing' }
-			format.xml  { render :xml => @components.to_xml }
+			format.xml  { render :xml => @computer.to_xml }
 		}
 	end
 
@@ -27,7 +27,7 @@ class ComputersController < ApplicationController
 		@testing = Testing.find(params[:testing_id])
 		respond_to { |format|
 			format.html { render :action => 'testing' }
-			format.xml  { render :xml => @testing.to_xml }
+			format.xml  { render :xml => @testing.to_xml(:include => :components) }
 		}
 	end
 
@@ -45,22 +45,56 @@ class ComputersController < ApplicationController
 		end
 	end
 
-  def edit
-    @computer = Computer.find(params[:id])
-  end
+	def edit
+		@computer = Computer.find(params[:id])
+	end
 
-  def update
-    @computer = Computer.find(params[:id])
-    if @computer.update_attributes(params[:computer])
-      flash[:notice] = 'Computer was successfully updated.'
-      redirect_to :action => 'show', :id => @computer
-    else
-      render :action => 'edit'
-    end
-  end
+	def set_assembler
+		@computer = Computer.find(params[:id])
+		@computer.assembler_id = params[:assembler_id]
+		@computer.assembling_start = Time.now
+		@computer.save!
+		head :ok
+	end
 
-  def destroy
-    Computer.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
+	def set_tester
+		@computer = Computer.find(params[:id])
+		@computer.tester_id = params[:tester_id]
+		@computer.save!
+		head :ok
+	end
+
+	def update
+		@computer = Computer.find(params[:id])
+		puts params[:id]
+		p params[:computer]
+		if @computer.update_attributes(params[:computer])
+			flash[:notice] = 'Computer was successfully updated.'
+			format.html { redirect_to :action => 'show', :id => @computer }
+			format.xml  { head :ok }
+		else
+			format.html { render :action => 'edit' }
+			format.xml  { render :xml => @computer.errors.to_xml }
+		end
+	end
+
+	def submit_components
+		@computer = Computer.find(params[:id])
+		d = REXML::Document.new(params[:list])
+		components = []
+		d.root.each_element { |c|
+			components << {
+				:type => c.elements['type'] ? c.elements['type'].text : '',
+				:vendor => c.elements['vendor'] ? c.elements['vendor'].text : '',
+				:model => c.elements['model'] ? c.elements['model'].text : '',
+				:serial => c.elements['serial'] ? c.elements['serial'].text : ''
+			}
+		}
+		p components
+	end
+
+	def destroy
+		Computer.find(params[:id]).destroy
+		redirect_to :action => 'list'
+	end
 end
