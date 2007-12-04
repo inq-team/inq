7b80bea3dd00cca5ff610b3ddc7abef5399c853b
Maxim Bartenev <bartenev@gmail.com>
2007-12-04 18:05:28 +0000

    * Updated testing overview * Fixed colour sequences for testing shelves * Fixed testing computers selection routines
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@326 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/models/computer.rb
+++ server/web/app/models/computer.rb
@@ -25,7 +25,7 @@ class Computer < ActiveRecord::Base
 	end
 
 	def self.find_testing()
-		self.find_by_sql(["SELECT computers.* FROM computers join computer_stages on computers.id = computer_stages.computer_id WHERE computer_stages.stage = 'testing' AND computer_stages.start <= now() AND (computer_stages.end IS NULL OR computer_stages.end > now())"])
+		self.find_by_sql(["SELECT distinct computers.* FROM computers join testings on computers.id = testings.computer_id WHERE testings.test_start <= now() AND (testings.test_end IS NULL OR testings.test_end > now())"])
 	end
 
 end
