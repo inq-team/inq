fd4382d488a8686a62dd7328f53b00a850a9f4fc
Maxim Bartenev <bartenev@gmail.com>
2007-12-05 11:17:55 +0000

    * Removed debug console output from shelves controller
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@331 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/controllers/shelves_controller.rb
+++ server/web/app/controllers/shelves_controller.rb
@@ -8,8 +8,7 @@ def addresses
 	if shelf
 		if shelf.ipnet =~ /(\d+)\.(\d+)\.(\d+)\.(\d+)\/(\d+)/
 			net = ($1.to_i() << 24) | ($2.to_i() << 16) | ($3.to_i() << 8) | ($4.to_i())
-			p net
-			render :text => (1..(1 << (32 - $5.to_i())) - 2).inject([]) { |a, i| p i ; a << (net | i) }.collect { |j| "#{j >> 24}.#{ (j >> 16) & 255 }.#{ (j >> 8) & 255 }.#{ j & 255 }" }.join("\n")
+			render :text => (1..(1 << (32 - $5.to_i())) - 2).inject([]) { |a, i| a << (net | i) }.collect { |j| "#{j >> 24}.#{ (j >> 16) & 255 }.#{ (j >> 8) & 255 }.#{ j & 255 }" }.join("\n")
 		else
 			raise("Malformed ip network address") 
 		end
