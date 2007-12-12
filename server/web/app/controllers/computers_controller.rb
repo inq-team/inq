e99efd525c8a177123f410dced8c13944d04128f
Maxim Bartenev <bartenev@gmail.com>
2007-12-12 16:08:46 +0000

    * Fixed a bug in testing overview where computer would not appear on a shelf
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@363 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/controllers/computers_controller.rb
+++ server/web/app/controllers/computers_controller.rb
@@ -88,7 +88,7 @@ class ComputersController < ApplicationController
 		config = Shelves::Config.new(params[:config]) if params[:config]
 		@computers = Computer.find_testing()
 		@shelves = config || @@default_config 
-		@byshelves = @computers.inject({}) { |h, c| h[@shelves.by_ipnet(c.ip) || c.shelf] = c ; h }
+		@byshelves = @computers.inject({}) { |h, c| sh = @shelves.by_ipnet(c.ip) ; h[!sh.blank? ? sh.full_name : c.shelf] = c ; h }
 		render(:layout => 'computer_shelves', :template => 'computers/shelves')				
 	end
 
