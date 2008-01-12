5450e02f805105838d5a60d440e3b9318c24b4b0
Mikhail Yakshin <greycat@altlinux.org>
2008-01-12 01:35:32 +0000

    Fixed previous migrations
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@410 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/db/migrate/001_create_order_lines.rb
+++ server/web/db/migrate/001_create_order_lines.rb
@@ -1,10 +1,13 @@
 class CreateOrderLines < ActiveRecord::Migration
-  def self.up
-    create_table :order_lines do |t|
-    end
-  end
+	def self.up
+		create_table :order_lines do |t|
+			t.column 'order_id', :integer, :null => false
+			t.column 'name',     :string,  :null => false, :limit => 250
+			t.column 'qty',      :integer, :null => false
+		end
+	end
 
-  def self.down
-    drop_table :order_lines
-  end
+	def self.down
+		drop_table :order_lines
+	end
 end
--- server/web/db/migrate/002_create_profiles.rb
+++ server/web/db/migrate/002_create_profiles.rb
@@ -1,10 +1,14 @@
 class CreateProfiles < ActiveRecord::Migration
-  def self.up
-    create_table :profiles do |t|
-    end
-  end
+	def self.up
+		create_table :profiles do |t|
+			t.column 'xml',       :text,     :null => false
+			t.column 'model_id' , :integer,  :null => true
+			t.column 'feature',   :string,   :null => true, :limit => 64
+			t.column 'timestamp', :datetime, :null => false
+		end
+	end
 
-  def self.down
-    drop_table :profiles
-  end
+	def self.down
+		drop_table :profiles
+	end
 end
