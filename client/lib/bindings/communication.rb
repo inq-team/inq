4dfa70b9914a28f76bd18d14300f386de280e070
Mikhail Yakshin <greycat@altlinux.org>
2008-02-12 15:52:05 +0000

    Non-background version of Ruby bindings by default
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@517 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- client/lib/bindings/communication.rb
+++ client/lib/bindings/communication.rb
@@ -1,17 +1,27 @@
 class Communication
 	attr_reader :sh
 
-	def initialize
-		@sh = IO::popen('/bin/sh', 'w')
-		@sh.puts("export COMPUTER_ID=#{ENV['COMPUTER_ID']}")
-		@sh.puts('. /etc/inquisitor/global')
-		@sh.puts('. $SHARE_DIR/functions')
-		@sh.puts('. $SHARE_DIR/communication')
+	def initialize(background = false)
+		if background then
+			@sh = IO::popen('/bin/sh', 'w')
+			@sh.puts("export COMPUTER_ID=#{ENV['COMPUTER_ID']}")
+			@sh.puts('. /etc/inquisitor/global')
+			@sh.puts('. $SHARE_DIR/functions')
+			@sh.puts('. $SHARE_DIR/communication')
+		else
+			@sh = nil
+		end
 	end
 
 	def method_missing(m, *args)
 		cmd = "#{m} " + args.map { |a| "'#{a}'" }.join(' ')
-#		puts cmd
-		@sh.puts(cmd)
+		if @sh then
+#			puts cmd
+			@sh.puts(cmd)
+		else
+			cmd = "export COMPUTER_ID=#{ENV['COMPUTER_ID']} && . /etc/inquisitor/global && . $SHARE_DIR/functions && . $SHARE_DIR/communication && " + cmd
+			puts cmd
+			system(cmd)
+		end
 	end
 end
