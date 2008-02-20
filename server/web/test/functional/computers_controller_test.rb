ed3112c8b8804288c844054e7faee077361cfb64
Mikhail Yakshin <greycat@altlinux.org>
2008-02-20 11:24:39 +0000

    Fixed test units
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@580 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/test/functional/computers_controller_test.rb
+++ server/web/test/functional/computers_controller_test.rb
@@ -25,16 +25,8 @@ class ComputersControllerTest < Test::Unit::TestCase
 
 	def test_plan_1
 		get :plan, :id => 1
+		res = assigns['pl'].plan.map { |t| t.type }
 		assert_response :success
-#		p assigns['pl'].script
-		assert_equal assigns['pl'].script, <<__EOF__
-PLANNER=1 TEST_NAME=cpu TESTTIME=1800 run_test cpu
-PLANNER=1 TEST_NAME=memory TEST_LOOPS=1 LOGTIME=120 run_test memory
-PLANNER=1 TEST_NAME=hdd-passthrough DISK_GROUP_SIZE=8 MINIMAL_STRESS_TIME=600 STRESS_TREE=/usr/share/inquisitor/linux-2.6.22.5-31-stress.tar.gz JOBS=16 RAMDISK_SIZE=400 run_test hdd-passthrough
-PLANNER=1 TEST_NAME=hdd-array JOBS=16 TIMEOUT=3600 STRESS_TREE=/usr/share/inquisitor/linux-2.6.22.5-31-stress.tar.gz LOGTIME=120 run_test hdd-array
-PLANNER=1 TEST_NAME=net URL=3000/test_file.html TIMEOUT=30 MD5=ca658fd4159bc35698edf9a1cdd70876 run_test net
-PLANNER=1 TEST_NAME=fdd FLOPPY_SIZE=1440 run_test fdd
-PLANNER=1 TEST_NAME=odd_read MESH_POINTS=1024 TEST_IMAGE_BLOCKS=50000 FORCE_NON_INTERACTIVE=false TEST_IMAGE_HASH=2e8744dfd11bf425001aad57976d42cc run_test odd_read
-__EOF__
+		assert_equal res, %w(cpu memory hdd-passthrough hdd-array net fdd odd_read)
 	end
 end
