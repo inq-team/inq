c2e313d4b9bfe271d2414e2708fd128dc7746d73
Mikhail Yakshin <greycat@altlinux.org>
2008-03-21 10:12:00 +0000

    Added missing indexes creation migration
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@735 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

---
+++ server/web/db/migrate/012_add_missing_indexes.rb
@@ -0,0 +1,16 @@
+class AddMissingIndexes < ActiveRecord::Migration
+	def self.up
+		add_index 'computers', ['model_id'], :name => 'model_id'
+		add_index 'components', ['testing_id'], :name => 'testing_id'
+		add_index 'components', ['component_model_id'], :name => 'component_model_id'
+		add_index 'marks', ['testing_stage_id'], :name => 'testing_stage_id'
+		add_index 'orders', ['buyer_order_number'], :name => 'buyer_order_number'
+	end
+	
+	def self.down
+		remove_index 'computers', 'model_id'
+		remove_index 'components', 'testing_id'
+		remove_index 'components', 'component_model_id'
+		remove_index 'orders', 'buyer_order_number'
+	end
+end
