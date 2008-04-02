84fed5a52eb839da82154ab49cf504086e22c32a
Mikhail Yakshin <greycat@altlinux.org>
2008-04-02 15:27:46 +0000

    Typo fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@810 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/lib/timespans.rb
+++ server/web/lib/timespans.rb
@@ -6,7 +6,7 @@ module Timespans
                 'acceptance' => 2 * 3600,
                 'assembling' => 6 * 3600,
                 'testing' => 48 * 3600,
-                'cheching' => 24 * 3600,
+                'checking' => 24 * 3600,
                 'packaging' => 24 * 3600,
         }
 
