44318c53c764b5a17859727099a05cbe0f888ccc
Mikhail Yakshin <greycat@altlinux.org>
2008-04-25 12:56:24 +0000

    Make overdue timing fall back gracefully if this stage has no overdue standard
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@894 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/controllers/orders_controller.rb
+++ server/web/app/controllers/orders_controller.rb
@@ -70,12 +70,14 @@ class OrdersController < ApplicationController
 
                 now = Time.new
                 @order_stages = @order.order_stages.find_all { |s| s.stage != 'manufacturing' }.inject([]) do |a, stage|
-                        a << {  :stage => stage.stage, :person => stage.person,
+			aa = {
+				:stage => stage.stage, :person => stage.person,
                                 :start => stage.start,
                                 :end => stage.end, :elapsed => (stage.end || now) - (stage.start || now),
-				:overdue => (stage.end || now) - (stage.start || now) > stage.default_timespan,
                                 :comment => stage.comment, :status => (stage.start.blank? || stage.start > now) ? :planned : stage.end ? :finished : :running
                         }
+			aa[:overdue] = (stage.end || now) - (stage.start || now) > stage.default_timespan if stage.default_timespan
+			a << aa
                 end.sort { |a, b| (a[:start] ? a[:start].to_f : 0) <=> (b[:start] ? b[:start].to_f : 0) }
 
                 ['ordering', 'warehouse', 'acceptance'].each do |stage_name|
