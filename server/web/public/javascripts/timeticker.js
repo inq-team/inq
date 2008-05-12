4d6a579a8c2b8da4757d9dc0b72d796fa397b6fe
Mikhail Yakshin <greycat@altlinux.org>
2008-05-12 11:44:00 +0000

    Added Javascript stage time ticker
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@940 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

---
+++ server/web/public/javascripts/timeticker.js
@@ -0,0 +1,20 @@
+function tick()
+{
+	var els = $$('tr.testing_stage_running td.testing_stage_span');
+	els.each(function(el, index) {
+		var st = el.innerHTML;
+		var h = parseInt(st.substring(0, 2));
+		var m = parseInt(st.substring(3, 5));
+		var s = parseInt(st.substring(6, 8));
+		s++;
+		if (s >= 60) { s = 0; m++; };
+		if (m >= 60) { m = 0; h++; };
+		el.update(h.toPaddedString(2) + ':' + m.toPaddedString(2) + ':' + s.toPaddedString(2));
+	});
+	setTimeout("tick()", 1000);
+}
+
+function startTicker()
+{
+	setTimeout("tick()", 1000);
+}
