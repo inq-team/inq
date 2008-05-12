d80a21948491eb12093a80c5650a75883373e3c5
Mikhail Yakshin <greycat@altlinux.org>
2008-05-12 11:47:39 +0000

    Fixed mysterious 08 loop bug due to deprecated octal parseInt javascript feature
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@941 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/public/javascripts/timeticker.js
+++ server/web/public/javascripts/timeticker.js
@@ -3,9 +3,9 @@ function tick()
 	var els = $$('tr.testing_stage_running td.testing_stage_span');
 	els.each(function(el, index) {
 		var st = el.innerHTML;
-		var h = parseInt(st.substring(0, 2));
-		var m = parseInt(st.substring(3, 5));
-		var s = parseInt(st.substring(6, 8));
+		var h = parseInt(st.substring(0, 2), 10);
+		var m = parseInt(st.substring(3, 5), 10);
+		var s = parseInt(st.substring(6, 8), 10);
 		s++;
 		if (s >= 60) { s = 0; m++; };
 		if (m >= 60) { m = 0; h++; };
