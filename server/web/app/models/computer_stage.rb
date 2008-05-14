2a502ae4fb7ef4add622b16c2e18ed8aacf72321
Mikhail Yakshin <greycat@altlinux.org>
2008-05-14 11:33:14 +0000

    Formatting fixes
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@947 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/models/computer_stage.rb
+++ server/web/app/models/computer_stage.rb
@@ -1,6 +1,5 @@
 class ComputerStage < ActiveRecord::Base
-        belongs_to :person, :class_name => 'Person', :foreign_key => 'person_id'
+	belongs_to :person, :class_name => 'Person', :foreign_key => 'person_id'
 
 	include Timespans
 end
-
