2211c5cd1c5b8742ef96f49f3fddd25b11121e2d
Sergey Matveev <stargrave@stargrave.org>
2008-05-23 11:46:29 +0000

    Ability to work with several image files.
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@972 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/controllers/firmwares_controller.rb
+++ server/web/app/controllers/firmwares_controller.rb
@@ -49,10 +49,9 @@ class FirmwaresController < ApplicationController
 		@firmware = Firmware.find(params[:id])
 		new_firmware = Firmware.new(params[:firmware])
 		updated_values = params[:firmware]
-		to_be_deleted = "#{FIRMWARES_DIR}/#{@firmware.image}"
 
 		if new_firmware.image != ""
-			orig = new_firmware.image.original_filename.to_s
+			orig = new_firmware.image.original_filename
 			if File.exists?("#{FIRMWARES_DIR}/#{orig}")
 				flash[:notice] = 'Such firmware image is already exists.'
 				render :action => 'edit'
@@ -61,7 +60,8 @@ class FirmwaresController < ApplicationController
 			image_file = File.new("#{FIRMWARES_DIR}/#{orig}", "wb")
 			image_file.write(new_firmware.image.read)
 			image_file.close
-			updated_values["image"] = orig
+			updated_values["image"] = @firmware.image + " " + orig
+			updated_values["image"].gsub!(/^ /, '')
 		end
 
 		updated_values.each_key { |x| updated_values.delete x if updated_values[x] == "" }
@@ -70,7 +70,6 @@ class FirmwaresController < ApplicationController
 			flash[:notice] = 'Such component model is already exists.'
 			render :action => 'edit'
 		elsif @firmware.update_attributes(updated_values)
-			File.delete to_be_deleted
 			flash[:notice] = 'Firmware was successfully updated.'
 			redirect_to :action => 'index'
 		else
@@ -80,7 +79,7 @@ class FirmwaresController < ApplicationController
 
 	def destroy
 		@firmware = Firmware.find(params[:id])
-		File.delete "#{FIRMWARES_DIR}/#{@firmware.image}"
+		@firmware.image.split(/ /).each { |f| File.delete "#{FIRMWARES_DIR}/#{f}" }
 		@firmware.destroy
 
 		redirect_to :action => 'index'
@@ -90,5 +89,18 @@ class FirmwaresController < ApplicationController
 		computer = Computer.find_by_id(params[:computer])
 		@components = computer ? computer.last_testing.components.collect { |x| [x.model.name, x.model.id] } : []
 		render(:layout => false)
+	end
+
+	def delete_file
+		@firmware = Firmware.find(params[:id])
+		file = params[:file]
+		update = @firmware.image.split(/ /)
+		update.delete(file)
+
+		File.delete "#{FIRMWARES_DIR}/#{file}"
+		@firmware.update_attribute("image", update.join(" ").gsub(/^ /, ''))
+
+		flash[:notice] = "#{file} successfully deleted."
+		render :action => 'edit'
 	end
 end
