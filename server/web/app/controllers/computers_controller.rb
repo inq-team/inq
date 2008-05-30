99f23be24505df56599d44393148f218301cb682
Mikhail Yakshin <greycat@altlinux.org>
2008-05-30 14:21:26 +0000

    More safety for random stages show
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@991 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/controllers/computers_controller.rb
+++ server/web/app/controllers/computers_controller.rb
@@ -738,7 +738,7 @@ __EOF__
                         a << {  :stage => stage.stage, :person => stage.person,
                                 :start => stage.start,
                                 :end => stage.end, :elapsed => (stage.end || now) - (stage.start || now),
-				:overdue => (stage.end || now) - (stage.start || now) > stage.default_timespan,
+				:overdue => stage.default_timespan ? (stage.end || now) - (stage.start || now) > stage.default_timespan : false,
                                 :comment => stage.comment, :status => (stage.start.blank? || stage.start > now) ? :planned : stage.end ? :finished : :running
                         }
                 end.sort { |a, b| (a[:start] ? a[:start].to_f : 0) <=> (b[:start] ? b[:start].to_f : 0) }
