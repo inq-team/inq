65ef3e824b7cf960bed63c2ee8033fab7d0d998f
Mikhail Yakshin <greycat@altlinux.org>
2008-05-30 07:43:08 +0000

    Straightened models controller; added layout
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@980 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/controllers/models_controller.rb
+++ server/web/app/controllers/models_controller.rb
@@ -1,51 +1,53 @@
 class ModelsController < ApplicationController
-  def index
-    list
-    render :action => 'list'
-  end
+	layout 'orders'
 
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
+	def index
+		list
+		render :action => 'list'
+	end
+
+	# GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+	verify :method => :post, :only => [ :destroy, :create, :update ],
+	       :redirect_to => { :action => :list }
 
 	def list
 		@models = Model.find(:all)
 	end
 
-  def show
-    @model = Model.find(params[:id])
-  end
+	def show
+		@model = Model.find(params[:id])
+	end
 
-  def new
-    @model = Model.new
-  end
+	def new
+		@model = Model.new
+	end
 
-  def create
-    @model = Model.new(params[:model])
-    if @model.save
-      flash[:notice] = 'Model was successfully created.'
-      redirect_to :action => 'list'
-    else
-      render :action => 'new'
-    end
-  end
+	def create
+		@model = Model.new(params[:model])
+		if @model.save
+			flash[:notice] = 'Model was successfully created.'
+			redirect_to :action => 'list'
+		else
+			render :action => 'new'
+		end
+	end
 
-  def edit
-    @model = Model.find(params[:id])
-  end
+	def edit
+		@model = Model.find(params[:id])
+	end
 
-  def update
-    @model = Model.find(params[:id])
-    if @model.update_attributes(params[:model])
-      flash[:notice] = 'Model was successfully updated.'
-      redirect_to :action => 'show', :id => @model
-    else
-      render :action => 'edit'
-    end
-  end
+	def update
+		@model = Model.find(params[:id])
+		if @model.update_attributes(params[:model])
+			flash[:notice] = 'Model was successfully updated.'
+			redirect_to :action => 'show', :id => @model
+		else
+			render :action => 'edit'
+		end
+	end
 
-  def destroy
-    Model.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
+	def destroy
+		Model.find(params[:id]).destroy
+		redirect_to :action => 'list'
+	end
 end
