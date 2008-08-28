3afa7f719d553eeb5892ca0799efb1c7eba8a5d9
Sergey Matveev <stargrave@stargrave.org>
2008-08-28 14:27:21 +0000

    Ability to show profile for one specified test. Small refactoring.
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@1299 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- client/main/create_profile_from_metainfo.pl
+++ client/main/create_profile_from_metainfo.pl
@@ -5,46 +5,33 @@ use warnings;
 
 use XML::Writer;
 
-my $i;
-my $x;
-my $exist;
-my @files;
-my @to_parse;
-
-die "Usage: create_profile_from_metainfo.pl directory_with_tests\n" if $#ARGV != 0;
+die "Usage: create_profile_from_metainfo.pl directory_with_tests [specified_test_name]\n" if $#ARGV < 0;
 
 # Read directory contents
-opendir DIR, $ARGV[0];
-@files = readdir DIR;
+opendir DIR, $ARGV[0] or die "Can not open directory with tests\n";
+my @files = grep { $_ = "$ARGV[0]/$_"; $_ if -f } readdir DIR;
 closedir DIR;
-map { $_ = $ARGV[0] . "/" . $_ } @files;
 
 # Begin XML
-$x = new XML::Writer(DATA_MODE => 1, DATA_INDENT => 1);
+my $x = new XML::Writer(DATA_MODE => 1, DATA_INDENT => 1);
 $x->startTag("tests");
 
-# We need only files
-for($i = 0; $i < $#files; $i++){
-	push @to_parse, $files[$i]
-	unless $files[$i] =~ /\.$/ or -d $files[$i];
-};
-
-foreach(@to_parse){
-	my $file = $_;
+foreach my $file (@files){
 	my $type = +(split '/', $file)[-1];
 
 	# Check for variables existence in metainformation
-	$exist = 0;
-	open FD, "< $_";
+	my $exist = 0;
+	open FD, "< $file";
 	while(<FD>){ /^# VAR=/ && $exist++; };
 	close FD;
-	$exist || next;
+	next unless $exist;
 
 	# Insert them into XML
+	next if defined $ARGV[1] and $type ne $ARGV[1];
 	$x->startTag("test", "id" => $type, "type" => $type);
 	open FD, "< $file";
 	while(<FD>){
-		/^# VAR=(.*):\w+:(.*):/ || next;
+		next unless /^# VAR=(.*):\w+:(.*):/;
 		$x->dataElement("var", "$2", "name" => $1);
 	};
 	close FD;
