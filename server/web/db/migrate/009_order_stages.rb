1f37da21563b5be4825cf857e24a06192cf667f7
Sergey Matveev <stargrave@stargrave.org>
2008-11-14 14:13:36 +0000

    Removed 009 migration that makes all database creation and migrations from an empty one perfectly working.
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@1373 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/db/migrate/009_order_stages.rb
+++
@@ -1,9 +0,0 @@
-class OrderStages < ActiveRecord::Migration
-	def self.up
-		add_index "order_stages", ["order_id"], :name => "order_id"
-	end
-	
-	def self.down
-		remove_index 'order_stages', :name => 'order_id'
-	end
-end
