29473df39a1d16c4bb78e2d0153a558e2dd3b2a1
Sergey Matveev <stargrave@stargrave.org>
2009-07-16 09:47:24 +0000

    Forgotten inq-software-detect command in motd.
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@1656 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- build/debian/live_lh_config.sh
+++ build/debian/live_lh_config.sh
@@ -90,6 +90,7 @@ You can use commands:
 
 ${COLOR_GREEN}inquisitor${COLOR_NORMAL} - execute Inquisitor testing in whole (normal testing mode)
 ${COLOR_GREEN}inq-detect${COLOR_NORMAL} - execute detects only
+${COLOR_GREEN}inq-software-detect${COLOR_NORMAL} - execute software detects only
 ${COLOR_GREEN}${SHARE_DIR}/test/${COLOR_YELLOW}STAGE${COLOR_NORMAL} - execute particular test ${COLOR_YELLOW}STAGE${COLOR_NORMAL}, one of:
 ${COLOR_BLUE}
 __EOF__
