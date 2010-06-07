470fe9b0be3267be63051d04593393954d6ffe10
Sergey Matveev <stargrave@stargrave.org>
2010-06-07 12:45:06 +0000

    * LiveHelper 1.xx is working again from trunk/ * LiveHelper 2.xx is able to build ISO now
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/branches/port-to-live-helper-2@1847 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- build/debian/live_lh_config.sh
+++ build/debian/live_lh_config.sh
@@ -21,8 +21,9 @@ fi
 # Run LiveHelper configuration utility itself
 ################################################################################
 pushd $WORKDIR/$LIVEDIR
-lh_config --mirror-bootstrap $REPO --mirror-chroot $REPO \
+lh config --mirror-bootstrap $REPO --mirror-chroot $REPO \
           --linux-flavours $KERNEL_FLAVOUR \
+          --linux-packages "linux-image-2.6" \
           --architecture $DEB_TARGET \
           --distribution $REPO_BRANCH \
           --iso-application Inquisitor \
@@ -32,24 +33,21 @@ lh_config --mirror-bootstrap $REPO --mirror-chroot $REPO \
           --bootappend-live "noautologin nolocales" \
           ${CUSTOM_KERNEL_OPTION} \
           --hostname inq \
-          --packages-lists inq \
-          --binary-indices disabled \
-          --apt-recommends disabled \
-          --apt-secure disabled \
+          --binary-indices none \
+          --apt-recommends false \
+          --apt-secure false \
           --bootstrap-flavour minimal \
           --chroot-filesystem squashfs \
-          --source disabled \
-          --union-filesystem aufs \
-          --bootloader grub \
+          --source false \
+          --packages-lists inq.list \
+          --bootloader syslinux \
           --bootstrap debootstrap \
-          --cache disabled \
-          --categories "$REPO_SECTIONS" \
-          --chroot-filesystem squashfs \
-          --union-filesystem aufs \
+          --cache false \
           --memtest memtest86+ \
-          --security disabled \
-          --checksums disabled \
-          --net-tarball none
+          --security false \
+          --checksums none \
+          --net-tarball none \
+          --apt-options='--allow-unauthenticated --yes'
 popd
 
 ################################################################################
@@ -60,7 +58,7 @@ echo "deb $REPO_MULTIMEDIA $REPO_BRANCH main" > $WORKDIR/$LIVEDIR/config/chroot_
 ################################################################################
 # Packages list, pressed file, Inquisitor package
 ################################################################################
-cp packages.live $WORKDIR/$LIVEDIR/config/chroot_local-packageslists/inq
+cp packages.live $WORKDIR/$LIVEDIR/config/chroot_local-packageslists/inq.list
 cp preseed.live $WORKDIR/$LIVEDIR/config/chroot_local-preseed/inq
 cp $WORKDIR/build-package/$PACKAGE_DEB $WORKDIR/$LIVEDIR/config/chroot_local-packages
 
