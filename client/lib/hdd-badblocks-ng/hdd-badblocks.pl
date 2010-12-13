d5affde16c5eb728986c9d4c1dbf28a0d61a457a
Sergey Matveev <stargrave@stargrave.org>
2010-12-13 03:07:24 +0000

    Do not warn about uninitialized empty variables
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@1859 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- client/lib/hdd-badblocks-ng/hdd-badblocks.pl
+++ client/lib/hdd-badblocks-ng/hdd-badblocks.pl
@@ -314,8 +314,8 @@ $BADBLOCKS_COMMAND .= defined $options{b} ? "-b $options{b} " : "-b 1024 ";
 $BADBLOCKS_COMMAND .= "-t $options{p} " if defined $options{p} and $options{p} ne "";
 $BADBLOCKS_COMMAND .= defined $options{o} ? "-c $options{o} " : "-c 64 ";
 for($options{m}){
-	if(/non-destructive/){ $BADBLOCKS_COMMAND .= "-n "; };
-	if(/^destructive/){ $BADBLOCKS_COMMAND .= "-w "; };
+	if( $_ and /non-destructive/ ){ $BADBLOCKS_COMMAND .= "-n "; };
+	if( $_ and /^destructive/ ){ $BADBLOCKS_COMMAND .= "-w "; };
 };
 print "Badblocks command: $BADBLOCKS_COMMAND\n";
 
