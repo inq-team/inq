ae53742ce121dad1a9947b8fb5c6f02ed712080f
Tatiana Bryleva <tanches@users.sourceforge.net>
2011-11-14 10:25:12 +0000

    added assemly statistic page
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@1969 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- server/web/app/controllers/statistics_controller.rb
+++ server/web/app/controllers/statistics_controller.rb
@@ -1,7 +1,7 @@
 class StatisticsController < ApplicationController
 	layout 'orders'
 
-	def index
+	def get_dates
 		if params[:date]
 			@from_day = params[:date]['from_day'].to_i
 			@from_month = params[:date]['from_month'].to_i
@@ -20,9 +20,12 @@ class StatisticsController < ApplicationController
 			@to_year = Date.today.year
 		end
 
-		from_date = Date.new( @from_year, @from_month, @from_day )
-		to_date = Date.new( @to_year, @to_month, @to_day )
+		@from_date = Date.new( @from_year, @from_month, @from_day )
+		@to_date = Date.new( @to_year, @to_month, @to_day )
+	end
 
+	def index
+		get_dates
 		available_assemblers = {}
 		@stats = {}
 		@model_stats = {}
@@ -30,7 +33,7 @@ class StatisticsController < ApplicationController
 		@total_stats = { :complexity => {},
 				 :computers => {} }
 		computers_seen = {}
-		ComputerStage.find_all_by_stage( "assembling", :conditions => ['end BETWEEN ? AND ?', from_date, to_date] ).each { |stage|
+		ComputerStage.find_all_by_stage( "assembling", :conditions => ['end BETWEEN ? AND ?', @from_date, @to_date] ).each { |stage|
 			computer = Computer.find_by_id( stage.computer_id )
 			next if not computer
 			computers_seen.has_key?( computer ) ? next : computers_seen[computer] = 1
@@ -70,4 +73,12 @@ class StatisticsController < ApplicationController
 	def rma
 		@rma_stat = Model.find_by_sql("Select m.*, count(c.id) total, count(s1.computer_id) checking, count(s2.computer_id) testing, avg(s1.d1) cavg, avg(s2.d2) tavg from models as m join computers as c on m.id = c.model_id left join ( Select computer_id, datediff(end, now() - interval 3 year) d1 from computer_stages where stage = \'checking\' group by computer_id having min( end) > now() - interval 3 year ) as s1 on c.id = s1.computer_id left join ( Select computer_id, datediff(end, now() - interval 3 year) d2 from computer_stages where stage = \'testing\' and computer_id not in ( Select computer_id from computer_stages where stage = \'checking\') group by computer_id having min( end) > now() - interval 3 year ) as s2 on c.id=s2.computer_id group by m.id order by m.name;")
 	end
+
+	def assembly
+		get_dates
+		@assembly_stat = ComputerStage.find_by_sql("Select DATE(cs.end) ddmmyy, COUNT(cs.end) comp_count, SUM(m.complexity)/60.0 hours, COUNT(DISTINCT(csa.person_id)) asmbl_count, SUM(m.complexity)/(60.0*8*COUNT(DISTINCT(csa.person_id))) coef from computer_stages AS cs JOIN computers AS c ON cs.computer_id=c.id JOIN models AS m on c.model_id=m.id JOIN computer_stages AS csa on cs.computer_id=csa.computer_id where cs.stage=\"checking\" AND csa.stage=\"assembling\" AND cs.start > \'#{@from_date}\' AND cs.end < \'#{@to_date}\' GROUP BY DATE(cs.end);")
+		@k = 0
+		@assembly_stat.each { |i| @k += i.coef.to_f if i.coef.to_f > 1}
+	end
+
 end
