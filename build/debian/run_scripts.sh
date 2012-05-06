694bb0821137c834d3124e8c38ea85a5ae722836
Mikhail Yakshin <greycat@altlinux.org>
2012-05-06 15:49:34 +0000

    Changed pushd/popd to just cd, as posix sh does not include pushd/popd
    git-svn-id: svn+ssh://svn.code.sf.net/p/inq/svn/trunk@2015 74654560-fcd8-4db3-b3c5-4c63e7cb38b4

--- build/debian/run_scripts.sh
+++ build/debian/run_scripts.sh
@@ -2,10 +2,9 @@
 
 export CHROOT_PATH REPO BRANCH SERVER DEBUG_TTY
 
-pushd build/debian/scripts
+cd build/debian/scripts
 for script in *; do
 	echo; echo "Running $script"
 	./$script
 	echo; echo "$script done"
 done
-popd
