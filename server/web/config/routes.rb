c1f7bf46a9fa8e03adc9b213702b09d3d3a104ac
Mikhail Yakshin <greycat@altlinux.org>
2013-10-22 12:41:59 +0400

    Converted little portion of existing routes to new DSL, more to go

--- server/web/config/routes.rb
+++ server/web/config/routes.rb
@@ -1,29 +1,14 @@
-Inquisitor::Application.routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
+Inquisitor::Application.routes.draw do
+#  map.connect ':controller/service.wsdl', :action => 'wsdl'
 
-  # Sample of regular route:
-  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
+  get 'computer/:action/:id/:testing', :to => 'computer'
 
-  # Sample of named route:
-  # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
+#  resources :orders do
+#    collection do
+#      get :testings, :staging
+#    end
+#    singular 'order'
+#  end
 
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  # map.connect '', :controller => "welcome"
-
-  # Allow downloading Web Service WSDL as a file with an extension
-  # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
-
-  map.connect 'computer/:action/:id/:testing', :controller => 'computer'
-
-  # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id'
-
-  map.resources :orders, :collection => { :testings => :get, :staging => :get }, :singular => 'order'
-  map.resources :computers
-#  map.resources :profiles
+  resources :computers
 end
