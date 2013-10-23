cc0bd24f949f9310c317ddbc1da72ee982998110
Mikhail Yakshin <greycat@altlinux.org>
2013-10-23 21:00:22 +0400

    Added explicit timespans require

--- server/web/app/models/computer_stage.rb
+++ server/web/app/models/computer_stage.rb
@@ -1,3 +1,5 @@
+require 'timespans'
+
 class ComputerStage < ActiveRecord::Base
 	belongs_to :person, :class_name => 'Person', :foreign_key => 'person_id'
 	belongs_to :comment_by, :class_name => 'Person', :foreign_key => 'comment_by'
