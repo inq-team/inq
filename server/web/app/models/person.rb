07790cfab29931c2bcc1842894383a5b44e876e4
Mikhail Yakshin <greycat@altlinux.org>
2013-10-24 18:40:51 +0400

    save_with_validation is deprecated in Rails 3.2 => replacing with regular save

--- server/web/app/models/person.rb
+++ server/web/app/models/person.rb
@@ -12,13 +12,13 @@ class Person < ActiveRecord::Base
 		self.remember_token_expires_at = 2.weeks.from_now
 		self.remember_token = Digest::SHA1.hexdigest("inq--#{self.login}--#{self.remember_token_expires_at}")
 #		self.password = ""  # This bypasses password encryption, thus leaving password intact
-		self.save_with_validation(false)
+		self.save(validate: false)
 	end
 
 	def forget_me
 		self.remember_token_expires_at = nil
 		self.remember_token = nil
 #		self.password = ""  # This bypasses password encryption, thus leaving password intact
-		self.save_with_validation(false)
+		self.save(validate: false)
 	end
 end
