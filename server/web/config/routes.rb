e7d58d215f5fee24129d557b7fe673caf0f964c0
Mikhail Yakshin <greycat@altlinux.org>
2013-10-24 12:52:08 +0400

    More route fixes to make computers/ work

--- server/web/config/routes.rb
+++ server/web/config/routes.rb
@@ -1,7 +1,12 @@
 Inquisitor::Application.routes.draw do
 #  map.connect ':controller/service.wsdl', :action => 'wsdl'
 
-  resources :computers
+  resources :computers do
+    member do
+      post :update_profile
+      get :sticker, :log, :mark, :graph, :ssh
+    end
+  end
   get 'computers/hw/:id', :to => 'computers#hw'
 
   resources :shelves do
