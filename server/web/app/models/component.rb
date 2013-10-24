b170a735c0eb076cf8e0e465d88fe86a870ddc2c
Mikhail Yakshin <greycat@altlinux.org>
2013-10-24 20:10:28 +0400

    Useful helper methods to get a new component by parameters

--- server/web/app/models/component.rb
+++ server/web/app/models/component.rb
@@ -4,4 +4,12 @@ class Component < ActiveRecord::Base
 	def===(other)
 		self.component_model_id == other.component_model_id and self.serial == other.serial
 	end
+
+	def self.by_params(params)
+		Component.new(
+			:serial => params[:serial],
+			:version => params[:version],
+			:model => ComponentModel.find_or_create_by_name_and_vendor_and_component_group_id(params[:model], params[:vendor], ComponentGroup.find_or_create_by_name(params[:type]).id)
+		)
+	end
 end
